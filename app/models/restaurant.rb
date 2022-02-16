46eea1cea960a0e8a80b63dc77d277c71cf60e34
Hugo Kubiak <hkubiak01@icloud.com>
2022-02-16 16:30:18 +0100

    ok

---
+++ app/models/restaurant.rb
@@ -0,0 +1,15 @@
+class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+
+  CATEGORIES = [
+    "chinese",
+    "italian",
+    "japanese",
+    "french",
+    "belgian"
+  ]
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :category, presence: true, inclusion: { in: CATEGORIES, message: 'chinese, italian, japanese, french or belgian' }
+end
